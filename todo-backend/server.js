30e1d6b4fbece94d52f79718a29c602f13148eff
SiphosethuMsengeni <siphosethu.msengeni@masinyusane.org>
2026-03-07 16:20:57 +0200

    Add Render deployment config for backend

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -47,6 +47,8 @@ app.put('/todos/:id', (req, res) => {
   res.json(todo);
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
